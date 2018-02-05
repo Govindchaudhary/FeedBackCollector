9ea23e45517b0e70a855b8e38c6b9c31828fb5eb
unknown <govindchaudhary2404@gmail.com>
2018-02-05 23:10:36 +0530

    changed heading

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=> {
-    res.send({hi:'there'});
+    res.send({hi:'i am awesome'});
 });
 
 const PORT = process.env.PORT || 5000 ;
