02a67bfef73703797363b8a88cb23d6b546218be
unknown <govindchaudhary2404@gmail.com>
2018-02-08 13:53:50 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
-app.get('/',(req,res)=> {
-    res.send({hi:'i am awesome'});
-});
+app.use(cookieSession({
+    maxAge:30*24*60*60*1000,
+    keys:[keys.cookieKey]
+})
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT); 
