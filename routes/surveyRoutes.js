64773ba9b24c46e1aa954715b68ef0d46d95ccd0
unknown <govindchaudhary2404@gmail.com>
2018-02-11 23:33:33 +0530

    survey routes added

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,28 @@
+const Survey = require('../models/survey');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
+module.exports = (app) => {
+   app.post('/api/surveys',requireLogin,requireCredits,async(req,res)=>{
+    const {title,subject,body,recipients} = req.body //ES6 syntax for title = req.body.title
+    const survey = new Survey({
+        title,  //ES6 syntax for title:title
+        subject,
+        body,
+        recipients:recipients.split(',').map(email=>{              /*  for recipients we have list of comma separated emails.
+                                                                    firstly we convert it into array of strings  and then 
+                                                                    with the map function convert into array of objects.
+                                                                  */
+            return{email:email}
+        }),
+        _user:req.user.id,
+        dateSent:Date.now()
+
+                                                        
+    });
+    const mailer = new Mailer(survey,surveyTemplate(survey));
+    await mailer.send();
+
+   }); 
+};
