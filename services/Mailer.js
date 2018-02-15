1bd20ae993bc29ad29e0cc2ee68ed9408d42aeba
unknown <govindchaudhary2404@gmail.com>
2018-02-15 23:23:28 +0530

    remove redundant function

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,12 +5,14 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
-
-    this.sgApi = sendgrid(keys.sendGridKey);
+    
+    
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
+    
     this.recipients = this.formatAddresses(recipients);
+    
 
     this.addContent(this.body);
     this.addClickTracking();
@@ -40,16 +42,7 @@ class Mailer extends helper.Mail {
     this.addPersonalization(personalize);
   }
 
-  async send() {
-    const request = this.sgApi.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: this.toJSON()
-    });
-
-    const response = await this.sgApi.API(request);
-    return response;
-  }
+  
 }
 
 module.exports = Mailer;
