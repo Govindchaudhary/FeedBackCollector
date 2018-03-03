63b18387b3c46cef1c2c709ff3028a4e6b99b2cc
unknown <govindchaudhary2404@gmail.com>
2018-03-03 14:59:27 +0530

    final touch to surveyTemplates

--- services/emailTemplates/surveyTemplates.js
+++ services/emailTemplates/surveyTemplates.js
@@ -8,8 +8,8 @@ module.exports = (survey) => {
               <p>please answer the following question:</p>
               <p>${survey.body}</p>
               <div>
-                <a href = "${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
-              </div>
+              <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
+           </div>
               <div>
                  <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
               </div>
