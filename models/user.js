a9e668436256ccf7c84ad5811c739983da2230d1
unknown <govindchaudhary2404@gmail.com>
2018-03-04 23:30:32 +0530

    two new field added

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    userName:String,
+    profile:String,
     googleId:String,
     credits:{type:Number,default:0}
 
