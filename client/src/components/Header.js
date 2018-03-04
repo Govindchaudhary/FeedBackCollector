3594ea1566304077c6f6fe4162e32fb15b22c50d
unknown <govindchaudhary2404@gmail.com>
2018-03-04 23:33:51 +0530

    added styling to navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,8 @@ import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
+       
+
         switch(this.props.user) {
             case null:
               return ;
@@ -14,18 +16,21 @@ class Header extends Component {
             return [
              <li key="1"> <Payments/></li>,
              <li key="3" style= {{margin:'0 10px'}}> Credits:{this.props.user.credits}</li>,
-            <li key="2"><a href="/api/logout" > Logout</a></li>
+            <li key="2"><a className="orange btn-flat white-text" href="/api/logout" > Logout</a></li>,
+            
             ];
         }
 }
     render() {
-        console.log(this.props.user);
+        
         
        return(
-           <nav>
+           <nav className="blue-grey " style={{height:'80px'}}>
+             
                <div className="nav-wrapper">
-                   <Link  to ={this.props.user?"/surveys":"/"} className="left brand-logo" >
-                     Emaily
+                   <Link  to ={this.props.user?"/surveys":"/"} className="left brand-logo" style= {{margin:'0 10px'}} >
+                     
+                     <spam style={{color:'#ffb74d'}}>Emaily</spam>
                     </Link>
                     <ul className = "right">
                         {this.renderContent()}
@@ -38,7 +43,9 @@ class Header extends Component {
 
 const mapStateToProps =(store)=> {
     return({
-        user:store.auth
+        user:store.auth,
+        
+
     });
 }
 export default connect(mapStateToProps) (Header);
