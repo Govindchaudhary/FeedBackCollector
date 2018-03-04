fc5d4a6ae5aa1d31ad68068d488230e38f74d4c7
unknown <govindchaudhary2404@gmail.com>
2018-03-04 23:32:43 +0530

    remove debug statements

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,15 @@ import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
-import reducers from './reducers';
+import {allReducers} from './reducers';
 import axios from 'axios';
 window.axios = axios;
 
 
-const store = createStore(reducers,{},applyMiddleware(reduxThunk));
+const store = createStore(allReducers,{},applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 <Provider store = {store}><App/></Provider>,
 document.querySelector('#root')
 );
 
-console.log('stripe key is',process.env.REACT_APP_STRIPE_KEY);
-console.log('environment is',process.env.NODE_ENV);
