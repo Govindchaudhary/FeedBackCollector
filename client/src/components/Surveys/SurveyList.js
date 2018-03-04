990cc83d45ec2aac0964d943b9609bcd0ed1e5f3
unknown <govindchaudhary2404@gmail.com>
2018-03-04 23:29:19 +0530

    user profile added

--- client/src/components/Surveys/SurveyList.js
+++ client/src/components/Surveys/SurveyList.js
@@ -7,12 +7,16 @@ class SurveyList extends Component {
     componentDidMount() {
         this.props.fetchSurveys();
     }
+    
 
     renderSurveys() {
         return this.props.surveys.reverse().map(survey => {
           return (
-            <div className="card darken-1" key={survey._id}>
-              <div className="card-content">
+           
+            <div className="row" >
+            <div className="col s12 m6">
+            <div className="card blue-grey darken-2" key={survey._id}>
+              <div className="card-content white-text">
                 <span className="card-title">{survey.title}</span>
                 <p>
                   {survey.body}
@@ -26,23 +30,52 @@ class SurveyList extends Component {
                 <a>No: {survey.no}</a>
               </div>
             </div>
+            </div>
+            </div>
+            
           );
         });
       }
       render() {
+        const str =this.props.profile ;
+        const myStr = str.replace(str.slice(-2),"150")
         return (
-          <div>
-            {this.renderSurveys()}
-          </div>
+          <div className="row">
+
+      <div className="col s3" style={{background:"#f5f5f5"}} >
+         
+         <div style={{marginTop:"40px",marginLeft:"70px"}}>
+            <img src={myStr}  style={{borderRadius:"8px"}} />
+            <spam ><p><strong>{this.props.userName}</strong></p></spam>
+            <div className="divider"></div>
+            <div >
+             <p style={{fontSize:"12px"}}>credits:{this.props.Credits}</p>
+            </div>
+            <div className="divider"></div>
+
+          <div >
+             <p style={{fontSize:"12px"}}>Settings</p>
+         </div>
+         <div></div>
+         </div>
+      </div>
+
+      <div class="col s9">
+        {this.renderSurveys()}
+      </div>
+
+    </div>
         );
       }
     }
 
 const mapStateToProps = (store) => {
-    return (
-        {
-            surveys:store.surveys
-        }
-    );
-}
+    return({
+            surveys:store.surveys,
+           userName:store.auth.userName,
+           profile:store.auth.profile,
+           Credits:store.auth.credits
+            
+        });
+};
 export default connect(mapStateToProps,actions)(SurveyList);
