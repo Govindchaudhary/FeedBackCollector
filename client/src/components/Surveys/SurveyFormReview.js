f54c16614c5a21a6cb364cea6dc7f6f3f3e7cc14
unknown <govindchaudhary2404@gmail.com>
2018-03-06 01:05:26 +0530

    survey review is styled

--- client/src/components/Surveys/SurveyFormReview.js
+++ client/src/components/Surveys/SurveyFormReview.js
@@ -8,18 +8,23 @@ import * as actions from '../../actions';
 const SurveyFormReview = (props) => {
     const reviewFields = _.map(formFields,(field)=>{
         return (
-            <div key={field.name}>
-                <label>{field.label}</label>
-                <div>
+            <div key={field.name}  >
+                <spam style={{color:"#212121"}}><label>{field.label}</label></spam>
+                <div style={{marginBottom:"10px"}}>
                     {props.formValues[field.name]}
                 </div>
             </div>
         )
     })
     return(
-        <div>
-           <h5> please confirm your entries</h5>
-           {reviewFields}
+        <div class="container" style={{marginTop:"40px"}}>
+           <spam style={{color:"#006064",textAlign:"center"}}><h5> please confirm your entries</h5></spam>
+           <div style={{marginTop:"25px",backgroundColor:"#f5f5f5",height:"215px"}}>
+           <div style={{margin:"10px"}}>
+            {reviewFields}
+           </div>
+           </div>
+           <div style={{marginTop:"30px"}}>
            <button
             className="yellow darken-3 btn-flat white-text"
             onClick= {props.onCancel} >
@@ -32,6 +37,7 @@ const SurveyFormReview = (props) => {
             
             <i className="material-icons right ">email</i>
            </button>
+           </div>
         </div>
 
     )
